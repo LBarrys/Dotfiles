a1060df79ef972ce53cda62c94f92a730cb7aa40
LBarrys <lbarrys@noreply.codeberg.org>
2025-12-27 19:57:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-PREREQUISITES="alacritty hyprland hyprpaper hyprland-scratchpad hyprpolkitagent waybar cliphist nwg-look rofi-wayland pcmanfm lxmenu-data file-roller pavucontrol gammastep lm_sensors fastfetch gruvbox-gtk-dark-medium-theme papirus-icon-theme papirus-folders breeze-cursor-theme ms-core-fonts google-noto-*"
+PREREQUISITES="alacritty hyprland hyprpaper hyprland-scratchpad hyprpolkitagent waybar cliphist nwg-look rofi-wayland pcmanfm lxmenu-data file-roller pavucontrol gammastep lm_sensors fastfetch gruvbox-gtk-dark-medium-theme papirus-icon-theme papirus-folders breeze-cursor-theme ms-core-fonts"
 
 # Ensure needed packages are installed
 if ! command -v $PREREQUISITES >/dev/null 2>&1; then
-    echo -e "\033[31mNeeded packages are not installed.\033[0m"
     sudo dnf copr enable lbarrys/nwg-look -y
     sudo dnf copr enable lbarrys/papirus-folders -y
     sudo dnf copr enable lbarrys/gruvbox-gtk-dark-medium-theme -y
@@ -12,13 +11,13 @@ if ! command -v $PREREQUISITES >/dev/null 2>&1; then
     sudo dnf copr enable lbarrys/hyprland -y
     sudo dnf copr enable lbarrys/hyprland-scratchpad -y
     sudo dnf copr enable lbarrys/ms-core-fonts -y
-    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-    sudo dnf install $PREREQUISITES --exclude=google-noto-*-vf-*,google-noto-fonts-all,google-noto-fonts-all-static,google-noto-fonts-all-vf
-    echo -e "\033[32mFinished installing needed packages.\033[0m"
+    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+    sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+    sudo dnf install $PREREQUISITES google-noto-* --exclude=google-noto-*-vf-*,google-noto-fonts-all,google-noto-fonts-all-static,google-noto-fonts-all-vf
 fi
 
 # Source bash file
-echo 'source "$HOME/.config/bash"' | sudo tee -a $HOME/.bashrc
+echo 'source "$HOME/.config/bash"' >> $HOME/.bashrc
 
 # Set Qt theme
 sudo mkdir -p /etc/environment.d/
@@ -30,8 +29,7 @@ echo 'FREETYPE_PROPERTIES="cff:no-stem-darkening=0 autofitter:no-stem-darkening=
 sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
 
 # Copy Dotfiles to their correct places
-cp -r $HOME/Dotfiles/dotconfig/* $HOME/.config
-cp -r $HOME/Dotfiles/dothome/* ~/
 mkdir -p $HOME/.local/bin
 cp -r $HOME/Dotfiles/dotlocal/* $HOME/.local/bin
+cp -r $HOME/Dotfiles/dotconfig/* $HOME/.config
 chmod 755 $HOME/.local/bin
